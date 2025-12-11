400aba904131779150f1d4be5dd054fbcffcc5eb
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-12-11 09:42:02 +0330

    started

---
+++ basics/hello-world.c
@@ -0,0 +1,10 @@
+// include keyword insert libraries and madules to your file 
+#include <stdio.h>
+
+// main function is the most important function in your code beacause everything you want to run must call in this function
+// void means you dont have any argument (input) in your function 
+// and int is the type of your function output
+int main(void)
+{
+
+}
---
+++ basics/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	// variables should start with type and that name like:
+	int age;
+	// you can value a var after declaring it like:
+	int count = 5;
+	
+	printf("Hello World!");
+}
